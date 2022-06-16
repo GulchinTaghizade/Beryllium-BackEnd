81efcadb6c5e4aa95476d20eabe8bd805e1afcd6
GulchinTaghizade <1pylr6h@code.edu.az>
2022-06-16 10:23:01 +0400

    10:22

---
+++ Beryllium_ASP.Net/wwwroot/assets/js/script.js
@@ -0,0 +1,34 @@
+
+$(document).on('ready', function () {
+    $(".regular").slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint:992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+});
